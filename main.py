2320e77d874e391d9a7c163f6c436250711542e4
Richard Li <richardli23@berkeley.edu>
2020-05-21 15:19:11 -0500

    fixed gcp cors bug

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
-from nba_api.stats.static import players
-from nba_api.stats.endpoints import playercareerstats
+from sportsreference.nba.roster import Player
 import json
-
-def getID(firstname, lastname=None):
-	try:
-		if lastname is None:
-			dic = players.find_players_by_first_name(firstname)
-		else:
-			dic = players.find_players_by_full_name(firstname + " " + lastname)
-		return dic[0]['id']
-	except:
-		raise Exception("Couldn't find player")
 
 def main(request):
 	request_json = request.get_json()
-	if request_json:
-		if "lastname" in request_json:
-			id = getID(request_json["firstname"], request_json["lastname"])
-		else:
-			id = getID(request_json["firstname"])
+	if request_json and "firstname" in request_json and "lastname" in request_json:
+		firstname, lastname = request_json["firstname"], request_json["lastname"]
 	else:
 		#fix this, should return some kind of error for user
 		return None
-		
-	career = playercareerstats.PlayerCareerStats(player_id=id)
-	return "Hello"
-	data = career.get_dict()
-	data = data['resultSets'][5]['rowSet'][0]
-	return "hello"
-	return str(data)
+
+	code = lastname[:5] + firstname[:2]
+	NN = 1
+	player = Player(code + str(0) + str(NN))
+	
+	while player.name.lower() != firstname + " " + lastname:
+		NN += 1
+		player = Player(code + str(0) + str(NN))
+
+	data = {
+		"points": player('2019-20').points,
+		"assists": player('2019-20').assists,
+		"blocks": player('2019-20').blocks
+	}
+
 	return json.dumps(data)
